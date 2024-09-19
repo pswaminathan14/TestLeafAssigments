2b7158b7b666f7c9192124af50f33b3d0d471880
pswaminathan14 <167870840+pswaminathan14@users.noreply.github.com>
2024-09-19 11:38:15 +0200

    Add files via upload
    Week 1 - Day 1

---
+++ AssignmentsWeek1/Chrome.java
@@ -0,0 +1,21 @@
+package AssignmentsWeek1;
+
+public class Chrome 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		float version=91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortKey ='C';
+		System.out.println("Chrome version: " +version);
+		System.out.println("Chrome developer: " +developer);
+		System.out.println("Chrome isBeta" +isBeta);
+		System.out.println("Chrome release year: " +releaseYear);
+		System.out.println("Chrome shortkey: " +shortKey);
+	}
+
+}
